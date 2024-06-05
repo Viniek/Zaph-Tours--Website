f0efe39b683b620a84ae378452562b21066284cc
unknown <njerivictory52@gmail.com>
2024-06-06 00:38:46 +0300

    ==

--- zaph tours/src/pages/Home/Header.jsx
+++ zaph tours/src/pages/Home/Header.jsx
@@ -1,62 +1,72 @@
+import React from 'react';
 import "./Header.css";
-
 import { Link } from "react-router-dom";
-function Header(){
-return(
-<>
-{/* navigation bar */}
-<div className="Top">    
-    <section className="orange">
-        <p>ppppp</p>
-    </section> 
-    <section className="topleft">
-        <img src="./src/assets/header.png"></img>
-        <p>Exemplary <br/>Tours</p>
-    </section>
-    <section className="header">
-    <p className="p1"><a href="/home">Home</a></p> 
-     <p className="p1"><a href="tours">Tours</a></p>
-    <p className="p1"><a href="destination">Destinations</a></p>
-    <p className="p1"><a href="contact us">Contact us</a></p>
-    <p className="p2">
-    <img src="./src/assets/phone.png"/><a href="vee">+254743209259</a></p> 
-    </section>
-    {/* nav bar */}
-    <div className="nav">
-        <img src="./src/assets/nav.png"/>
-        <div className="navhover">
-            <div className="navhover1">
-            <h1>Your gateway to unforgettable adventures and breathtaking destinations!</h1>
-            <p>Embark on a journey with us and discover the world like never before!</p>
-            <div className="navhovericons">
-                <img src="./src/assets/holidays.png"/>
-                <p>Holidays</p>
-                <img src="./src/assets/phone.png"/>
-                <p>+254743209259</p>
-                <img src="./src/assets/gmail.png"/>
-                <p>njerivictory52@gmail.com</p>
-                <div className="navhoverlist">
-                    <ul className="navdestinations">
-                        <li>Mt. Kenya</li>
-                        <li>Mt. Ruwenzori</li>
-                        <li>Lake Nakuru</li>
-                        <li>Tsavo National Park</li>
-                        <li>Nairobi national park</li>
-                        <li> Coastal Kenya</li>
-                    </ul>
+
+// Corrected import paths
+import headerImage from '../../assets/header.png';
+import navImage from '../../assets/nav.png';
+import phoneIcon from '../../assets/phone.png';
+import holidaysIcon from '../../assets/holidays.png';
+import gmailIcon from '../../assets/gmail.png';
+
+function Header() {
+    return (
+        <>
+            {/* navigation bar */}
+            <div className="Top">
+                <section className="orange">
+                    <p>ppppp</p>
+                </section>
+                <section className="topleft">
+                    <img src={headerImage} alt="Header" />
+                    <p>Exemplary <br />Tours</p>
+                </section>
+                <section className="header">
+                    <p className="p1"><a href="/home">Home</a></p>
+                    <p className="p1"><a href="/tours">Tours</a></p>
+                    <p className="p1"><a href="/destination">Destinations</a></p>
+                    <p className="p1"><a href="/contact us">Contact us</a></p>
+                    <p className="p2">
+                        <img src={phoneIcon} alt="Phone" /><a href="vee">+254743209259</a>
+                    </p>
+                </section>
+                {/* nav bar */}
+                <div className="nav">
+                    <img src={navImage} alt="Nav" />
+                    <div className="navhover">
+                        <div className="navhover1">
+                            <h1>Your gateway to unforgettable adventures and breathtaking destinations!</h1>
+                            <p><i>Embark on a journey with us and discover the world like never before!</i></p>
+                            <div className="navhovericons">
+                                <img src={holidaysIcon} alt="Holidays" />
+                                <p>Holidays</p>
+                                <img src={phoneIcon} alt="Phone" />
+                                <p>+254743209259</p>
+                                <img src={gmailIcon} alt="Gmail" />
+                                <p>njerivictory52@gmail.com</p>
+                                <div className="navhoverlist">
+                                    <ul className="navdestinations">
+                                        <li>Mt. Kenya</li>
+                                        <li>Mt. Ruwenzori</li>
+                                        <li>Lake Nakuru</li>
+                                        <li>Tsavo National Park</li>
+                                        <li>Nairobi National Park</li>
+                                        <li>Coastal Kenya</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-{/* body */}
-<section className="body">
-    <div className="video">
-        <p><i>Discover The charm of unexplored destinations</i></p>
-    </div>
-</section>
-</>
-)
+            {/* body */}
+            <section className="body">
+                <div className="video">
+                    <p><i>Discover The charm of unexplored destinations</i></p>
+                </div>
+            </section>
+        </>
+    );
 }
+
 export default Header;
