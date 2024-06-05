2aa28a91c67a35eac60a1e9ff024a89aa9f39183
unknown <njerivictory52@gmail.com>
2024-06-05 12:50:13 +0300

    --

--- zaph tours/src/pages/Home/Header.jsx
+++ zaph tours/src/pages/Home/Header.jsx
@@ -9,7 +9,7 @@ return(
     <p1><a href="tours">Tours</a></p1>
     <p1><a href="destination">Destinations</a></p1>
     <p1><a href="contact us">Contact us</a></p1>
-    <p1>Call Us today:<a href="vee">+254743209259</a></p1>
+    <p2>Call Us today:<a href="vee">+254743209259</a></p2>
 </div>
 </>
 )
