9b7ac3ba31c22a4cfec12be42a3718ed2a0e49ef
unknown <njerivictory52@gmail.com>
2024-06-08 04:22:26 +0300

    -

--- zaph tours/src/pages/Home/Header.jsx
+++ zaph tours/src/pages/Home/Header.jsx
@@ -87,32 +87,7 @@ function Header() {
                 </div>
             </section>
 
-            {/* Icons */}
-            <div className='icons'>
-                <img src={kenyaicon}alt="mt.Kenya"/>
-                <img src={nakuruicon}alt="nakuru"/>
-                <img src={kilimanjaroicon}alt="mt.Kilimanjaro"/>                
-                <img src={tsavoicon}alt="tsavo"/>
-                <img src={coasticon}alt="coast"/>
-                <img src={nairobiicon}alt="nairobi"/>      
-               
-            </div>
-        
-            {/* about us */}
-            <section className='about_us'>
-                <div className="big">
-                    
-                    <img src={bigimage} alt='big' className='background_image'/>
-                        <img src={small} alt="small" className='overlay_image'/>                    
-                </div>
-                <div className='about_us_right'>
-                    <h1><i>Who we are</i></h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.Distinctio,
-                         iusto<br/> voluptatum repudiandae vitae veritatis nulla?<br/> Repellendus accusantium 
-                         veniam corporis soluta id non provident <br/>molestias maiores nobis, nostrum unde
-                          earum quibusdam.</p>
-                </div>
-            </section>
+           
             <section className='Testimonies'>
                 <div className='person'>
                     <h1>Jonathan Muiruri</h1>
@@ -189,7 +164,7 @@ function Header() {
 </section>
 <section className='form'>
 
-    
+    <label
 </section>
 </section>
 
---
+++ zaph tours/src/pages/Home/Hero.jsx
@@ -0,0 +1,13 @@
+ 
+ 
+ {/* Icons */}
+ <div className='icons'>
+ <img src={kenyaicon}alt="mt.Kenya"/>
+ <img src={nakuruicon}alt="nakuru"/>
+ <img src={kilimanjaroicon}alt="mt.Kilimanjaro"/>                
+ <img src={tsavoicon}alt="tsavo"/>
+ <img src={coasticon}alt="coast"/>
+ <img src={nairobiicon}alt="nairobi"/>      
+
+</div>
+
