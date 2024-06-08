51b3d3ce9ad0d7f84400d53c6e0a32a3035d6885
unknown <njerivictory52@gmail.com>
2024-06-08 05:12:28 +0300

    --

--- zaph tours/src/App.jsx
+++ zaph tours/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter,Routes, Route} from "react-router-dom";
-import Home from "./pages/Home/Home";
+import Home from "./pages/Homehome/Home";
 import Destinations from "./pages/Destinations/Destination";
 import Contact_us from "./pages/Contact_us/Contact";
 import Trips_type from "./pages/Trips_type/Trips";
+import Header from "./pages/Homehome/Header";
+import Footer from "./pages/Homehome/Footer";
 
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
+        <Footer />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Destination" element={<Destinations/>}/>
@@ -18,8 +21,8 @@ function App() {
           <Route path="/Trips" element={<Trips_type />} />         
         </Routes>
       </BrowserRouter>
-      <Footer />
-    </>
+      
+          </>
   );
 }
 
--- zaph tours/src/pages/Destinations/Destination.jsx
+++ zaph tours/src/pages/Destinations/Destination.jsx
@@ -1,9 +1,77 @@
 import "./Destination.css";
+import mtkenya from '../../assets/mtkenya.jpg';
+import mtkilimanjaro from '../../assets/mtkilimanjaro.jpg';
+import lakenakuru from '../../assets/nakuru.jpg';
+import coast from '../../assets/coast.jpg';
+import tsavo from '../../assets/tsavo.jpg';
+import nairobipark from '../../assets/nairobi park.jpg';
+import { Link } from "react-router-dom";
+
 
 function Destination(){
     return(
         <>
-        <p>vee</p>
+        <section className="destinations">
+            <div className="destination1">
+                {/* 1 */}
+                <div className="tsavo">
+                    <img src={mtkenya}alt="mountain"/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque exercitationem facilis obcaecati numquam fuga,
+                         alias blanditiis nesciunt nisi consectetur in doloribus quisquam debitis nemo repudiandae perspiciatis voluptas
+                          quam iusto autem.<br/>$20 per group<br/> $10per head</p>
+                          <Link to="https://github.com/Viniek">Mt.Kenya</Link>
+                          <h1>Mt.Kenya</h1>
+                </div>
+                {/* 2 */}
+                <div className="tsavo">
+                    <img src={mtkilimanjaro}alt="mountain"/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque exercitationem facilis obcaecati numquam fuga,
+                         alias blanditiis nesciunt nisi consectetur in doloribus quisquam debitis nemo repudiandae perspiciatis voluptas
+                          quam iusto autem.<br/>$20 per group<br/> $10per head</p>
+                          <Link to="https://github.com/Viniek">Mt.Kilimanjaro</Link>
+                          <h1>Mt.Kilimanjaro</h1>
+                </div>
+                {/* 3 */}
+                <div className="tsavo">
+                    <img src={lakenakuru}alt="lake"/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque exercitationem facilis obcaecati numquam fuga,
+                         alias blanditiis nesciunt nisi consectetur in doloribus quisquam debitis nemo repudiandae perspiciatis voluptas
+                          quam iusto autem.<br/>$20 per group<br/> $10per head</p>
+                          <Link to="https://github.com/Viniek">Lake Nakuru</Link>
+                          <h1>Lake Nakuru</h1>
+                </div>
+         
+            </div>
+            <div className="destination2">
+                       {/* 4 */}
+                       <div className="tsavo">
+                    <img src={coast}alt="coast"/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque exercitationem facilis obcaecati numquam fuga,
+                         alias blanditiis nesciunt nisi consectetur in doloribus quisquam debitis nemo repudiandae perspiciatis voluptas
+                          quam iusto autem.<br/>$20 per group<br/> $10per head</p>
+                          <Link to="https://github.com/Viniek">Coast</Link>
+                          <h1>Coast</h1>
+                </div>
+                {/* 5 */}
+                <div className="tsavo">
+                    <img src={tsavo}alt="mountain"/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque exercitationem facilis obcaecati numquam fuga,
+                         alias blanditiis nesciunt nisi consectetur in doloribus quisquam debitis nemo repudiandae perspiciatis voluptas
+                          quam iusto autem.<br/>$20 per group<br/> $10per head</p>
+                          <Link to="https://github.com/Viniek">Tsavo</Link>
+                          <h1>Tsavo</h1>
+                </div>
+                {/* 6 */}
+                <div className="tsavo">
+                    <img src={nairobipark}alt="park"/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque exercitationem facilis obcaecati numquam fuga,
+                         alias blanditiis nesciunt nisi consectetur in doloribus quisquam debitis nemo repudiandae perspiciatis voluptas
+                          quam iusto autem.<br/>$20 per group<br/> $10per head</p>
+                          <Link to="https://github.com/Viniek">Narobi National Park</Link>
+                          <h1>Nairobi National Park</h1>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
